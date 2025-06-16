0ff69b0402043325372f08c82090ac6b44602906
salva <scasconbertomeu@al.uloyola.es>
2025-06-16 18:31:32 +0200

    Eliminar usuario frontend

--- app/src/main/java/com/fitlife/conexionServer/FitLifeService.java
+++ app/src/main/java/com/fitlife/conexionServer/FitLifeService.java
@@ -39,6 +39,8 @@ import retrofit2.http.Part;
 import retrofit2.http.Query;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.DELETE;
+
 
 public interface FitLifeService {
 
@@ -144,4 +146,6 @@ public interface FitLifeService {
     @POST("api/analyze")
     Call<AnalisisResponse> analizaEtiqueta(@Body Map<String,String> body);
 
+    @DELETE("api/eliminar-usuario")
+    Call<GenericResponse> eliminarUsuario();
 }
